76de1c4d281df993dec5d88a0e803b658602be2d
徐文兵 <330262107@qq.com>
2016-11-24 20:22:24 +0800

    dialog更新

--- IAWLibrary/src/main/java/com/iaskwind/iawlibrary/tools/IAW_DialogTool.java
+++ IAWLibrary/src/main/java/com/iaskwind/iawlibrary/tools/IAW_DialogTool.java
@@ -4,6 +4,7 @@ import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.os.Build;
+import android.view.View;
 
 /**
  * Created by winston on 16/8/5.
@@ -12,8 +13,50 @@ public class IAW_DialogTool {
     public interface DialogListener{
         void ok();
         void cancle(DialogInterface dialog);
+        void cancle();
     }
 
+//    public interface DialogCallBack {
+//        void ok();
+//
+//        void cancle();
+//    }
+
+    public static void showSysDialog(Context mContext, String title, View view, String msg, final DialogListener dialogCallBack) {
+        AlertDialog.Builder builder;
+        if (Build.VERSION.SDK_INT > Build.VERSION_CODES.GINGERBREAD_MR1) {
+            builder = new AlertDialog.Builder(mContext, AlertDialog.THEME_HOLO_LIGHT);
+        } else {
+            builder = new AlertDialog.Builder(mContext);
+        }
+        builder.setTitle(title);
+        if (view != null) {
+            builder.setView(view);
+        }
+        if (msg != null) {
+            builder.setMessage(msg);
+        }
+        builder.setNegativeButton("取消", new DialogInterface.OnClickListener() {
+
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+                dialog.dismiss();
+                dialogCallBack.cancle();
+
+            }
+        }).setPositiveButton("确定", new DialogInterface.OnClickListener() {
+
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialogCallBack.ok();
+
+            }
+
+        });
+        AlertDialog dlg = builder.create();
+        dlg.show();
+    }
     public static void showDialog(Context mContext, String title, String msg, final DialogListener listener){
         AlertDialog.Builder builder;
         if (Build.VERSION.SDK_INT > Build.VERSION_CODES.GINGERBREAD_MR1) {
